3f6cc33e6caeed7f1297f9f18afc4ea76b3318e4
Rohan9026 <rohan.cool18786@gmail.com>
2024-10-29 00:23:56 +0530

    inter added

---
+++ inter/src/data.js
@@ -0,0 +1,55 @@
+
+const data =[
+    {
+        id:1,
+        name:"Agra",
+        info:"Agra is a city on the banks of the Yamuna river in the Indian state of Uttar Pradesh, about 230 kilometres (140 mi) south-east of the national capital Delhi and 330 km west of the state capital Lucknow. With a population of roughly 1.6 million, Agra is the fourth-most populous city in Uttar Pradesh and twenty-third most populous city in India.",
+        image:require("./images/agra.jpg"),
+        price:"8,000",
+    },
+    {
+        id:2,
+        name:"Jaipur",
+        info:"Jaipur formerly known as Jeypore, is the capital and the largest city of the Indian state of Rajasthan. As of 2011, the city has a population of 3.1 million, making it the tenth most populous city in the country. Located 268 km (167 miles) from the national capital New Delhi, Jaipur is also known as the Pink City due to the dominant color scheme of its buildings in old city.",
+        image:require("./images/jaipur.jpg"),
+        price:"7,000",
+    },
+    {
+        id:3,
+        name:"Goa",
+        info:"Goa is a state on the southwestern coast of India within the Konkan region, geographically separated from the Deccan highlands by the Western Ghats.[7][8] It is bound by the Indian states of Maharashtra to the north, and Karnataka to the east and south, with the Arabian Sea in the west. It is India's smallest state by area and fourth-smallest by population. Goa has the highest GDP per capita among all Indian states,[4][9] two and a half times as high as the GDP per capita of the country as a whole.",
+        image:require("./images/goa.jpg"),
+        price:"15,000",
+    },
+    {
+        id:4,
+        name:"Varanasi",
+        info:"Varanasi or Kashi  is a city on the Ganges river in northern India that has a central place in the traditions of pilgrimage, death, and mourning in the Hindu world.The city has a syncretic tradition of Muslim artisanship that underpins its religious tourism.",
+        image:require("./images/varanasi.jpg"),
+        price:"10,000",
+    },
+    {
+        id:5,
+        name:"Darjeeling",
+        info:"Darjeeling is a city in the northernmost region of the Indian state of West Bengal. Located in the Eastern Himalayas, it has an average elevation of 2,045 metres (6,709 ft).[10] To the west of Darjeeling lies the easternmost province of Nepal, to the east the Kingdom of Bhutan, to the north the Indian state of Sikkim, and farther north the Tibet Autonomous Region region of China",
+        image:require("./images/darjeeling.jpg"),
+        price:"12,000",
+    },
+    {
+        id:6,
+        name:"Jaisalmer",
+        info:"Jaisalmer nicknamed The Golden city, is a city in the Indian state of Rajasthan, located 575 kilometres (357 mi) west of the state capital Jaipur. It is the administrative headquarters of Jaisalmer District. Before Indian independence, the town served as the capital of the Jaisalmer State, ruled by the Bhati Rajputs.[7] Jaisalmer stands on a ridge of yellowish sandstone and is crowned by the ancient Jaisalmer Fort.",
+        image:require("./images/jaisalmer.jpg"),
+        price:"7,500",
+    },
+    {
+        id:7,
+        name:"Kochi",
+        info:"Kochi, also known by its former name Cochin is a major port city along the Malabar Coast of India bordering the Laccadive Sea. It is part of the district of Ernakulam in the state of Kerala. The city is also commonly referred to as Ernakulam. As of 2011, the Kochi Municipal Corporation had a population of 677,381 over an area of 94.88 km2, and the larger Kochi urban agglomeration had over 2.1 million inhabitants within an area of 440 km2, making it the largest and the most populous metropolitan area in Kerala.",
+        image:require("./images/kochi.jpg"),
+        price:"20,000",
+    },
+
+];
+
+export default data;
